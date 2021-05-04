241fbe83a03fa5f56173b412e6cdb2c9a77115f9
khiem90 <khiemduy90@gmail.com>
2021-05-04 18:31:43 -0500

    Styling login form.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,28 +1,64 @@
 import { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router"
-import FirebaseContext from '../context/firebase';
+import { useHistory } from "react-router";
+import FirebaseContext from "../context/firebase";
 
 export default function Login() {
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword]  = useState('');
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [error, setError] = useState('');
-    const isInvalid = password === ''  || emailAddress === '';
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
 
+  const handleLogin = () => {};
 
-    const handleLogin = () => {};
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  }, []);
 
-    useEffect(() => {
-        document.title = 'Login - Instagram';
-    }, []);
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iphone with profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="flex justify-center w-full">
+          <img
+            src="/images/logo.png"
+            alt="instagram logo"
+            className="mt-2 w-6/12"
+          />
+        </h1>
+        {error && <p className="mb-4 text-xs test-red-primary">{error}</p>}
 
-
-    return (
-        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <p>I am the login page</p>
-        </div>
-    )
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            aria-label="Enter Your Email Address"
+            type="text"
+            placeholder="Email Address"
+            className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <input
+            aria-label="Enter Your Password"
+            type="password"
+            placeholder="Password"
+            className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button
+            disabled={isInvalid}
+            type="submit"
+            className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
+              isInvalid && `opacity-50`
+            }`}
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/seed.js
+++ src/seed.js
@@ -1,70 +1,79 @@
 export function seedDatabase(firebase) {
-    const users = [
-      {
-        userId: 'pQ5C40eV1GTXBSSpufyxMBORx493',
-        username: 'Chimeinwhenever',
-        fullName: 'Khiem Nguyen',
-        emailAddress: 'Khiemduy90@gmail.com',
-        following: ['2'],
-        followers: ['2', '3', '4'],
-        dateCreated: Date.now()
-      },
-      {
-        userId: '2',
-        username: 'raphael',
-        fullName: 'Raffaello Sanzio da Urbino',
-        emailAddress: 'raphael@sanzio.com',
-        following: [],
-        followers: ['pQ5C40eV1GTXBSSpufyxMBORx493'],
-        dateCreated: Date.now()
-      },
-      {
-        userId: '3',
-        username: 'dali',
-        fullName: 'Salvador Dalí',
-        emailAddress: 'salvador@dali.com',
-        following: [],
-        followers: ['pQ5C40eV1GTXBSSpufyxMBORx493'],
-        dateCreated: Date.now()
-      },
-      {
-        userId: '4',
-        username: 'orwell',
-        fullName: 'George Orwell',
-        emailAddress: 'george@orwell.com',
-        following: [],
-        followers: ['pQ5C40eV1GTXBSSpufyxMBORx493'],
-        dateCreated: Date.now()
-      }
-    ];
-  
-    for (let k = 0; k < users.length; k++) {
-      firebase.firestore().collection('users').add(users[k]);
-    }
-  
-    for (let i = 1; i <= 5; i++) {
-      firebase
-        .firestore()
-        .collection('photos')
-        .add({
-          photoId: i,
-          userId: '2',
-          imageSrc: `/images/users/raphael/${i}.jpg`,
-          caption: 'Saint George and the Dragon',
-          likes: [],
-          comments: [
-            {
-              displayName: 'dali',
-              comment: 'Love this place, looks like my animal farm!'
-            },
-            {
-              displayName: 'orwell',
-              comment: 'Would you mind if I used this picture?'
-            }
-          ],
-          userLatitude: '40.7128°',
-          userLongitude: '74.0060°',
-          dateCreated: Date.now()
-        });
-    }
+  const users = [
+    {
+      userId: "pQ5C40eV1GTXBSSpufyxMBORx493",
+      username: "Splinter",
+      fullName: "Splinter Sensei",
+      emailAddress: "Splinter@sensei.com",
+      following: ["2"],
+      followers: ["2", "3", "4"],
+      dateCreated: Date.now(),
+    },
+    {
+      userId: "2",
+      username: "Raphael",
+      fullName: "Raphael Turtle",
+      emailAddress: "Raphael@turtle.com",
+      following: [],
+      followers: ["pQ5C40eV1GTXBSSpufyxMBORx493"],
+      dateCreated: Date.now(),
+    },
+    {
+      userId: "3",
+      username: "Leonardo",
+      fullName: "Leonardo Turtle",
+      emailAddress: "Leonardo@turtle.com",
+      following: [],
+      followers: ["pQ5C40eV1GTXBSSpufyxMBORx493"],
+      dateCreated: Date.now(),
+    },
+    {
+      userId: "4",
+      username: "Donatello",
+      fullName: "Donatello Turtle",
+      emailAddress: "Donatello@turtle.com",
+      following: [],
+      followers: ["pQ5C40eV1GTXBSSpufyxMBORx493"],
+      dateCreated: Date.now(),
+    },
+    {
+      userId: "5",
+      username: "Michelangelo",
+      fullName: "Michelangelo Turtle",
+      emailAddress: "Michelangelo@turtle.com",
+      following: [],
+      followers: ["pQ5C40eV1GTXBSSpufyxMBORx493"],
+      dateCreated: Date.now(),
+    },
+  ];
+
+  for (let k = 0; k < users.length; k++) {
+    firebase.firestore().collection("users").add(users[k]);
   }
+
+  for (let i = 1; i <= 6; i++) {
+    firebase
+      .firestore()
+      .collection("photos")
+      .add({
+        photoId: i,
+        userId: "2",
+        imageSrc: `/images/users/raphael/${i}.jpg`,
+        caption: "Saint George and the Dragon",
+        likes: [],
+        comments: [
+          {
+            displayName: "Raphael",
+            comment: "Love this place, looks like my animal farm!",
+          },
+          {
+            displayName: "Michelangelo",
+            comment: "Would you mind if I used this picture?",
+          },
+        ],
+        userLatitude: "40.7128°",
+        userLongitude: "74.0060°",
+        dateCreated: Date.now(),
+      });
+  }
+}
