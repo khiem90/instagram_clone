b8acd240c6ab26148ffc965f15a906cd9adbab53
khiem90 <khiemduy90@gmail.com>
2021-05-10 13:58:50 -0500

    Login page (#1)
    * Setup tailwindcss and resolved eslint issues.
    
    * Styling login form.
    
    * Handle login success and error.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,17 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 // Import seed file here
-// import { seedDatabase } from '../seed';
+// import { seedDatabase } from "../seed";
 
 const config = {
-    apiKey: "AIzaSyC_SKYNytB5ojzZXiaZx1lssSXQsRvCn2c",
-    authDomain: "instagramclone-702ed.firebaseapp.com",
-    projectId: "instagramclone-702ed",
-    storageBucket: "instagramclone-702ed.appspot.com",
-    messagingSenderId: "568158916428",
-    appId: "1:568158916428:web:fda24334ba83bd520289e9"
+  apiKey: "AIzaSyC_SKYNytB5ojzZXiaZx1lssSXQsRvCn2c",
+  authDomain: "instagramclone-702ed.firebaseapp.com",
+  projectId: "instagramclone-702ed",
+  storageBucket: "instagramclone-702ed.appspot.com",
+  messagingSenderId: "568158916428",
+  appId: "1:568158916428:web:fda24334ba83bd520289e9",
 };
 
 const firebase = Firebase.initializeApp(config);
