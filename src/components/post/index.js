65357e051a77c1f005b31c8072efdb76221b3050
khiem90 <khiemduy90@gmail.com>
2021-06-18 04:02:39 -0500

    Added toggle like and display image.

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,10 +1,28 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
+import Header from "./header";
+import Image from "./image";
+import Actions from "./actions";
 
 export default function Post({ content }) {
-  console.log(content);
-  const { caption } = content;
-  return <p>{caption}</p>;
+  const commentInput = useRef(null);
+
+  const handleFocus = () => commentInput.current.focus();
+
+  console.log(handleFocus);
+
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+      <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+    </div>
+  );
 }
 
 Post.propTypes = {
