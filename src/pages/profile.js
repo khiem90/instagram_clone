f390eb49347a9f64d513cf97df8dfda2a7eeebcc
khiem90 <khiemduy90@gmail.com>
2021-07-14 05:32:03 -0500

    Checked if user exist.

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,32 @@
+import { useParams, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getUserByUsername } from "../services/firebase";
+import * as ROUTES from "../constants/routes";
+
 export default function Profile() {
-  return <p>testing</p>;
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const [userExist, setUserExist] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExist() {
+      const user = await getUserByUsername(username);
+      if (user.length > 0) {
+        setUser(user[0]);
+        setUserExist(true);
+      } else {
+        setUserExist(false);
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExist();
+  }, [username, history]);
+
+  return userExist ? (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg">{user.fullName}</div>
+    </div>
+  ) : null;
 }
